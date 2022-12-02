2cd7f1714f0bc910d08182fbed1de7f434edb8f0
WAGUNA-SS <waguna_saresu@hotmail.com>
2022-12-02 13:23:22 -0300

    att do menu hamburguinho

--- EXCERCICIOS/ex026/mq005/js/clickmenu.js
+++ EXCERCICIOS/ex026/mq005/js/clickmenu.js
@@ -5,3 +5,11 @@ function clickMenu(){
 else {
     itensmenu.style.display = 'block'}
 }
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itensmenu.style.display = 'block'}
+
+else {
+    itens.style.display = 'none'} 
+}
